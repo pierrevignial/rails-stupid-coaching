7b2ccdd72d404ceefbef739c473a22ef6b0c7ae5
Pierre Vignial <pierrevignial3@gmail.com>
2021-02-08 12:52:55 +0100

    got it

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @coach_answer = coach_answer(params[:question])
+  end
+end
+
+def coach_answer(your_message)
+  if your_message.include?("?")
+    return "Silly question, get dressed and go to work!"
+  elsif your_message == "I am going to work right now!"
+    return ""
+  else
+    return "I don\'t care, get dressed and go to work!"
+  end
+end
